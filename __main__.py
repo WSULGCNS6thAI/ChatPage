8b871d8afb3a8f4be3a2ff717ecacf3809456f73
komool <67896210+komool@users.noreply.github.com>
2022-11-07 10:15:44 +0900

    change var name

--- __main__.py
+++ __main__.py
@@ -15,9 +15,9 @@ def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
 @socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event : ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+def handle_my_custom_event(json_msg, methods=['GET', 'POST']):
+    print('received my event : ' + str(json_msg))
+    socketio.emit('my response', json_msg, callback=messageReceived)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
