541f26683333b02f4374986f2777f133b5013040
kimjongmu <116610800+kimjongmu@users.noreply.github.com>
2022-11-15 17:13:26 +0900

    Create komoran.py

---
+++ py/komoran.py
@@ -0,0 +1,41 @@
+from socket  import *
+import threading
+import time
+from konlpy.tag import Komoran
+komoran = Komoran()
+userInput = '' # 22.11.14 김경호
+
+def send(sock):
+    while True:
+        sendData = input('>>>')
+        sock.send(sendData.encode('utf-8'))
+
+def receive(sock):
+    while True:
+        recvData = sock.recv(1024)
+        global userInput # 22.11.14 김경호
+        userInput = recvData.decode('utf-8') # 22.11.14 김경호
+        print('상대방 :', userInput) # 22.11.14 김경호
+        print(komoran.nouns(userInput))
+
+port = 8081
+
+serverSock = socket(AF_INET, SOCK_STREAM)
+serverSock.bind(('', port))
+serverSock.listen(1)
+
+print('%d번 포트로 접속 대기중...'%port)
+
+connectionSock, addr = serverSock.accept()
+
+print(str(addr), '에서 접속되었습니다.')
+
+sender = threading.Thread(target=send, args=(connectionSock,)) #send함수를 인자를 넣어 쓰레드 생성
+receiver = threading.Thread(target=receive, args=(connectionSock,)) #receive함수를 인자를 넣어 쓰레드 생성
+
+sender.start()
+receiver.start()
+
+while True: #프로그램을 계속 실행한다
+    time.sleep(1) #1초 쉰다
+    pass
