b3d6f1408a63bc66c9152afcf6be98fd2d68910a
komool <67896210+komool@users.noreply.github.com>
2022-11-21 19:48:07 +0900

    Create answer_query.py

---
+++ answer_query.py
@@ -0,0 +1,13 @@
+# 22.11.21 김경호
+import pymysql
+conn = pymysql.connect(host='127.0.0.1', user='root', password='qlalfqjsgh1!',db='chatdb',charset='utf8') 
+
+def search(arr):
+    cur = conn.cursor()
+
+    with cur:
+        cur.execute('USE chatdb;')
+        cur.execute('SELECT DISTINCT level4.question FROM level1, level2, level3, level4 WHERE level1.id = {0} and level2.id = {1} and level3.id = {2} and level4.table3_id = {2};'.format(arr[0], arr[1], arr[2]))
+
+    conn.commit()
+    conn.close()
