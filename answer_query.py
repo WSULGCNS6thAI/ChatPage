d39f1d4af973726964b9dbe60c5fe31edef6956e
komool <67896210+komool@users.noreply.github.com>
2022-11-24 10:50:54 +0900

    Update answer_query.py

--- answer_query.py
+++ answer_query.py
@@ -8,9 +8,9 @@ def search(arr):
     with cur:
         cur.execute('USE chatdb;')
         cur.execute('SELECT DISTINCT level4.question FROM level1, level2, level3, level4 WHERE level1.id = {0} and level2.id = {1} and level3.id = {2} and level4.table3_id = {2};'.format(arr[0], arr[1], arr[2]))
-        result = cur.fetchall()
+        result = cur.fetchone() # 22.11.24 김경호
 
     conn.commit()
     conn.close()
 
-    return result
+    return result # 22.11.24 김경호
